266e0a4c98e8922501e1656d79de9519ec0644c4
Albatros-code <albert.kolakowski@gmail.com>
2019-11-10 12:29:35 +0100

    Updates

---
+++ precache-manifest.0617784d118e9a930b05344c0416c418.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25665cb91c4d1a2ad8e6078c25b9b433",
+    "url": "/shopping-list-app/index.html"
+  },
+  {
+    "revision": "2d3578b932bda6e8d9f3",
+    "url": "/shopping-list-app/static/js/2.bc9ea9d8.chunk.js"
+  },
+  {
+    "revision": "88cdd64a82570242fcc6",
+    "url": "/shopping-list-app/static/js/main.fd4d2c05.chunk.js"
+  },
+  {
+    "revision": "0f45ce24562f12c862a6",
+    "url": "/shopping-list-app/static/js/runtime-main.304f2973.js"
+  }
+]);
